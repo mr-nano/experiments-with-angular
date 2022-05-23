9aaca27f599e45676decfbad1cdb0b4bbe50711e
nano <4840723+mr-nano@users.noreply.github.com>
2022-05-23 13:34:28 +0000

    Add. Add task is clickable, yet to actually add task though

--- angular-crash/src/app/components/button/button.component.ts
+++ angular-crash/src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,11 +7,16 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
   @Input() text: string = "Click me";
-  @Input() backgroundColor : string = "black";
+  @Input() backgroundColor: string = "black";
+  @Output() buttonClicked = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClick() {
+    this.buttonClicked.emit();
   }
 
 }
