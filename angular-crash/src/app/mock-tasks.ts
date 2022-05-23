81c4be481dbbb2c900746719bb6c3fa6edb40fc5
nano <4840723+mr-nano@users.noreply.github.com>
2022-05-23 13:49:15 +0000

    Add. Mock data and interface for tasks

---
+++ angular-crash/src/app/mock-tasks.ts
@@ -0,0 +1,34 @@
+import {Task} from './Task';
+
+export const TASKS : Task[] = [
+  {
+    id: 1,
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    day: '2018-04-12',
+    reminder : true
+  },
+  {
+    id: 2,
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    day: '2018-04-12',
+    reminder : true
+  },
+  {
+    id: 3,
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    day: '2018-04-12',
+    reminder : true
+  },
+  {
+    id: 4,
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    day: '2018-04-12',
+    reminder : true
+  },
+  {
+    id: 5,
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    day: '2018-04-12',
+    reminder : true
+  }
+];
